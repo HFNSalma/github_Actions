e06df4b212cc21643a11ed8923885cfc2664526d
HAFIANI Salma <hafianisalma820@gmail.com>
2024-05-22 19:23:31 +0100

    Mon premier commit

---
+++ app/src/main/java/com/example/tp2_1/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.tp2_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.view.View;
+
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvHello;
+    private EditText etName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        tvHello = findViewById(R.id.tvHELLO);
+
+        etName = findViewById(R.id.eTName);
+    }
+    public void onClick(View view) {
+        if(etName.getText().length()==0){
+            tvHello.setText(R.string.txtsalut);
+        }else{
+            String str = etName.getText().toString();
+            String strr = getString(R.string.txtsalut1) +" "+str;
+            tvHello.setText(strr);
+        }
+    }
+}
+
+
